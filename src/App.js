0e241b9ebe57d2212c95a992a7ae11ea56f9f30c
Arthur Cabral <63428806+Ah-Cabral@users.noreply.github.com>
2021-07-14 23:59:32 -0300

    React App Initial Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Footer from "./Components/Footer/Footer";
+import Header from "./Components/Header/Header";
+import "./Styles/Main.css"
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
