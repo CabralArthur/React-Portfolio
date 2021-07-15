e299cd0db26e66339a63826e7f1fe065ec8ff8a3
Arthur Cabral <63428806+Ah-Cabral@users.noreply.github.com>
2021-07-15 00:47:04 -0300

    Adding HomeSection

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Footer from "./Components/Footer/Footer";
 import Header from "./Components/Header/Header";
+import HomeSection from "./Components/HomeSection/HomeSection";
 import "./Styles/Main.css"
 
 function App() {
   return (
     <div className="App">
       <Header/>
-
+      <HomeSection/>
       <Footer/>
     </div>
   );
