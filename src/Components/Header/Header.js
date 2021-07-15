278caaa715532ec6e5bf8fe937532934f5520e08
Arthur Cabral <63428806+Ah-Cabral@users.noreply.github.com>
2021-07-15 00:19:33 -0300

    Implementing Header

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import "../../Styles/Main.css"
+import "./Header.css"
+
+const Header = () => {
+    return <>
+        <header className='container header'>
+            <span className='name-span'>Arthur<span className='orange-span'>Cabral</span> </span>
+            <nav className='header-nav'>
+                <ul>
+                    <li>
+                        <a href='/'>About</a>
+                    </li>
+                    <li>
+                        <a href='/'>Projects</a>
+                    </li>
+                    <li>
+                        <a href='/'>Contact</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    </>
+}
+
+export default Header;
