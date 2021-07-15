0a697492735e434d43977009a4b1f54360906341
Arthur Cabral <63428806+Ah-Cabral@users.noreply.github.com>
2021-07-15 00:20:01 -0300

    Adding (Initial) Footer

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../../Styles/Main.css"
+//import "./Styles/Footer.css"
+
+const Footer = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Footer
