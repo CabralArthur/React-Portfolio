be3f831f0d81e3ebf8f645d848a216ddab653521
Arthur Cabral <63428806+Ah-Cabral@users.noreply.github.com>
2021-07-15 22:55:20 -0300

    Adding title component

--- src/Components/HomeSection/HomeSection.js
+++ src/Components/HomeSection/HomeSection.js
@@ -2,19 +2,17 @@ import React from 'react'
 import "../../Styles/Main.css"
 import "./HomeSection.css"
 import DevImage from '../../assets/img/vector/dev-vector.svg'
+import Title from '../Title/Title'
 
 const HomeSection = () => {
     return (
         <div className='container homeSection'>
             <div className='homeText'>
-                <h1>
-                    Beginner / FullStack
-                    Developer.
-                </h1>
+                <Title texto='Beginner / FullStack Developer'/>
                 <span>
                     Student of Information Systems, immersed in the world of technology since 2020..
                 </span>
-                <a href='/' className='primaryButton'>Saiba Mais ;)</a>
+                <a href='/' className='primaryButton'>Know More ;)</a>
             </div>
             <div className='homeImage'>
                 <img src={DevImage} className='devImage' alt='Imagem Developer'>
