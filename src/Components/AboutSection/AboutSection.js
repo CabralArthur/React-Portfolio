ad95c635c3ebbd0439143d585330c90d806dda3f
Arthur Cabral <63428806+Ah-Cabral@users.noreply.github.com>
2021-07-17 01:04:24 -0300

    Finishing About Section

--- src/Components/AboutSection/AboutSection.js
+++ src/Components/AboutSection/AboutSection.js
@@ -2,15 +2,16 @@ import React from 'react'
 import "../../Styles/Main.css"
 import "./AboutSection.css"
 import AboutDevImage from '../../assets/img/vector/about-dev-vector.svg'
+import Title from '../Title/Title'
 
 const AboutSection = () => {
     return (
         <section className='aboutSection container'>
-            <h2>&lt;About <span className='orange-span'> Me</span> /&gt;</h2>
+            <Title titulo='About Me' subtitulo='My introduction'/>
             <div className='aboutSectionContent'>
-                <img src={AboutDevImage}></img>
+                <img src={AboutDevImage} alt='About Section DevImage'></img>
                 <div className='aboutSectionText'>
-                    <span>Graduated in Administration, enthusiast in Front-End Development, UX/Design, Photoshop edition, Brand style, landing page Design</span> 
+                    <span>Graduating in Information Systems at UFRPE, student passionate about technology.</span> 
                     <button className='primaryButton'>
                         Download My CV <i class="fas fa-download"></i>
                     </button>
