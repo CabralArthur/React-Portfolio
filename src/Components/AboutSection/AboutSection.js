85c251a63f53889705ca643384c0ce7cc0349b65
Arthur Cabral <63428806+Ah-Cabral@users.noreply.github.com>
2021-07-18 00:32:11 -0300

    Finishing About Section

--- src/Components/AboutSection/AboutSection.js
+++ src/Components/AboutSection/AboutSection.js
@@ -2,21 +2,33 @@ import React from 'react'
 import "../../Styles/Main.css"
 import "./AboutSection.css"
 import AboutDevImage from '../../assets/img/vector/about-dev-vector.svg'
-import Title from '../Title/Title'
 
 const AboutSection = () => {
     return (
-        <section className='aboutSection container'>
-            <Title titulo='About Me' subtitulo='My introduction'/>
+        <section className='aboutSection'>
+            <div className='sectionTitle'>
+                <h1 className='aboutTitle'>
+                    About Me
+                </h1>
+                <h3 className='aboutSubtitle'>
+                    Know More About Me
+                </h3>
+            </div>
             <div className='aboutSectionContent'>
-                <img src={AboutDevImage} alt='About Section DevImage'></img>
+
+                <div className='imageWrapper'>
+                    <img src={AboutDevImage} alt='About Section DevImage'></img>
+                </div>
+
                 <div className='aboutSectionText'>
                     <span>Graduating in Information Systems at UFRPE, student passionate about technology.</span> 
-                    <button className='primaryButton'>
-                        Download My CV <i class="fas fa-download"></i>
-                    </button>
+                    <a className='primaryButton' href='/'>
+                        Download My CV <i className="fas fa-download"></i>
+                    </a>
                 </div>
+                
             </div>
+
         </section>
     )
 }
