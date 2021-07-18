87a82848be61220baba778cff195245f2823df4b
Arthur Cabral <63428806+Ah-Cabral@users.noreply.github.com>
2021-07-18 18:07:15 -0300

    Creating Footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,12 +1,26 @@
 import React from 'react'
 import "../../Styles/Main.css"
-//import "./Styles/Footer.css"
+import "./Footer.css"
 
 const Footer = () => {
     return (
-        <div>
-            
-        </div>
+        <footer className='footerSite'>
+            <span>Portfolio Arthur Cabral</span>
+            <span> Todos os Direitos Reservados &copy; - 2020  </span>
+            <span className='iconsFooter'>
+                <a>
+                    <i className="fab fa-whatsapp-square"></i>
+                </a>
+
+                <a>
+                    <i className="fas fa-envelope-square"></i>
+                </a>
+                
+                <a>
+                    <i className="fab fa-linkedin"></i>
+                </a>
+            </span>
+        </footer>
     )
 }
 
