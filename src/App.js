a530b816c6f4b42c066783564d0daefa5c74270a
Arthur Cabral <63428806+Ah-Cabral@users.noreply.github.com>
2021-07-18 00:34:14 -0300

    Adding container in app and adding Portfolio Section

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import AboutSection from "./Components/AboutSection/AboutSection";
 import Footer from "./Components/Footer/Footer";
 import Header from "./Components/Header/Header";
 import HomeSection from "./Components/HomeSection/HomeSection";
+import Portfolio from "./Components/Portfolio/Portfolio";
 import SkillsSection from "./Components/SkillsSection/SkillsSection";
 import "./Styles/Main.css"
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container">
       <Header/>
       <HomeSection/>
       <AboutSection/>
       <SkillsSection/>
+      <Portfolio/>
       <Footer/>
     </div>
   );
